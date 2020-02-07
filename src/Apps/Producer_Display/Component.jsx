d6ccf012396d2d67e25fdc701625d1830f1f9d49
Amaka Anikwe <amakaanikwe@gmail.com>
2020-02-06 21:38:39 -0500

    Updated comp

--- src/Apps/Producer_Display/Component.jsx
+++ src/Apps/Producer_Display/Component.jsx
@@ -5,10 +5,10 @@ class ProducerComp extends React.Component{
       
     render () {
       return (
-        <div className="container-fluid menuStyle">
+        <div className="container-fluid">
           <div className='row'>
             <div className='col-lg-12'>
-              <div className='sectionStyle'>Coffee & Tea</div>
+              <div>Coffee & Tea</div>
             </div>
           </div>
           <table className='row'>
@@ -17,21 +17,10 @@ class ProducerComp extends React.Component{
                 {this.props.menu.coffee.map((item, i) => <CoffeeTeaComp key = {i} coffee = {item} />)}
             </tbody>
           </table>
-          <div className='row'>
-            <div className='col-sm-3'>
-            <br></br>
-            <br></br>
-              <div className='sectionStyle'>Treats</div>
-            </div>
-          </div>
-
-          <button onClick={()=>this.props.onCompToggle()}> 
-        {!this.props.turnCompToggleOn ? 'Order Now' : 'Back to Menu'}
-          </button>
         </div>
       )
     }
   
   };
   
-  export default MenuComp;
+  export default ProducerComp;
