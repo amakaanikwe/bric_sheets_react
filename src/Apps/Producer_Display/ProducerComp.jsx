2777425a1cdec75022ad11eb68ca089dfaf856a8
Amaka Anikwe <amakaanikwe@gmail.com>
2020-02-08 23:46:30 -0500

    Updated comp

--- src/Apps/Producer_Display/ProducerComp.jsx
+++ src/Apps/Producer_Display/ProducerComp.jsx
@@ -5,24 +5,24 @@ class ProducerComp extends React.Component{
       
     render () {
       return (
-        <section className="container containerStyle">
+        <section className="container">
           <div className="row">
             <div className="col-lg-8">
-              <h3>{person.firstName + " " + person.lastName}</h3>
+              <h3>{this.props.firstName + " " + this.props.lastName}</h3>
             </div>
-            <div className="col-lg-4">{person.emailAddress}</div>
+            <div className="col-lg-4">{this.props.emailAddress}</div>
           </div>
           <div className="row">
             <h5 className="col-lg-12">Bric Certifications</h5>
             <div className="col-lg-8">
               <ul>
-                <li>{ person.bricCertification}</li>
+                <li>{ this.props.bricCertification}</li>
               </ul>
             </div>
             <h5 className="col-lg-12">Work Experience</h5>
             <div className="col-lg-8">
               <ul>
-                <li>{person.workExperience}</li>
+                <li>{this.props.workExperience}</li>
               </ul>
               <div>
             <a>
