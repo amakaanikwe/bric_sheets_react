575447fb2556528bbdae4adf584ef8029b750634
Amaka Anikwe <amakaanikwe@gmail.com>
2020-02-08 23:33:25 -0500

    Updated producer comp

--- src/Apps/Producer_Display/ProducerComp.jsx
+++ src/Apps/Producer_Display/ProducerComp.jsx
@@ -5,28 +5,28 @@ class ProducerComp extends React.Component{
       
     render () {
       return (
-        <section class="container containerStyle" ng-repeat="person in filteredProducers |orderBy: 'lastName' ">
-          <div class="row">
-            <div class="col-lg-8">
+        <section className="container containerStyle">
+          <div className="row">
+            <div className="col-lg-8">
               <h3>{person.firstName + " " + person.lastName}</h3>
             </div>
-            <div class="col-lg-4">{person.emailAddress}</div>
+            <div className="col-lg-4">{person.emailAddress}</div>
           </div>
-          <div class="row">
-            <h5 class="col-lg-12">Bric Certifications</h5>
-            <div class="col-lg-8">
+          <div className="row">
+            <h5 className="col-lg-12">Bric Certifications</h5>
+            <div className="col-lg-8">
               <ul>
                 <li>{ person.bricCertification}</li>
               </ul>
             </div>
-            <h5 class="col-lg-12">Work Experience</h5>
-            <div class="col-lg-8">
+            <h5 className="col-lg-12">Work Experience</h5>
+            <div className="col-lg-8">
               <ul>
                 <li>{person.workExperience}</li>
               </ul>
               <div>
-            <a ng-click="openModal(person)">
-            <button type="button" class="btn-info">details</button>
+            <a>
+            <button type="button" className="btn-info">details</button>
           </a>
         </div>
       </div>
