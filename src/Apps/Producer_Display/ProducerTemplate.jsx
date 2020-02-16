728c151e57eec255594beb76d99380553e564096
Amaka Anikwe <amakaanikwe@gmail.com>
2020-02-15 23:39:51 -0500

    Added pagination

--- src/Apps/Producer_Display/ProducerTemplate.jsx
+++ src/Apps/Producer_Display/ProducerTemplate.jsx
@@ -5,33 +5,38 @@ class ProducerTempComp extends React.Component{
       
     render () {
       return (
-        <section className="container containerStyle borderBottom">
-          <div className="row">
-            <div className="col-lg-9">
-              <h3>{this.props.producer.firstName + " " + this.props.producer.lastName}</h3>
+          <React.Fragment>
+            <section className="container containerStyle borderBottom">
+            <div className="row">
+                <div className="col-lg-9">
+                <h3>{this.props.producer.firstName + " " + this.props.producer.lastName}</h3>
+                </div>
+                <div className="col-lg-3">{this.props.producer.emailAddress}</div>
             </div>
-            <div className="col-lg-3">{this.props.producer.emailAddress}</div>
-          </div>
-          <div className="row">
-            <h5 className="col-lg-12">Bric Certifications</h5>
-            <div className="col-lg-8">
-              <ul>
-                <li>{this.props.producer.bricCertification}</li>
-              </ul>
+            <div className="row">
+                <h5 className="col-lg-12">Bric Certifications</h5>
+                <div className="col-lg-8">
+                <ul>
+                    <li>{this.props.producer.bricCertification}</li>
+                </ul>
+                </div>
+                <h5 className="col-lg-12">Work Experience</h5>
+                <div className="col-lg-8">
+                <ul>
+                    <li>{this.props.producer.workExperience}</li>
+                </ul>
+                <div>
+                <a>
+                <button type="button" className="btn-info">details</button>
+            </a>
             </div>
-            <h5 className="col-lg-12">Work Experience</h5>
-            <div className="col-lg-8">
-              <ul>
-                <li>{this.props.producer.workExperience}</li>
-              </ul>
-              <div>
-            <a>
-            <button type="button" className="btn-info">details</button>
-          </a>
         </div>
-      </div>
-    </div>
-    </section>
+        </div>
+        </section>
+        <section class="container" >
+        <pagination uib-pagination ng-model="currentPage" total-items="producers.length" max-size="maxSize" items-per-page="numPerPage" boundary-links="true"  class="pagination-sm pagination" previous-text="&lsaquo;" next-text="&rsaquo;" first-text="&laquo;" last-text="&raquo;"></pagination>
+        </section>
+    </React.Fragment>
       )
     }
   
