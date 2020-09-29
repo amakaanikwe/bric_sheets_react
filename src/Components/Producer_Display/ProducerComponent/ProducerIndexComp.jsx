832ff514c2993205be40f9a3e309e498c81c80fb
Amaka Anikwe <amakaanikwe@gmail.com>
2020-09-29 08:11:50 -0400

    Updated ProducerIndexComp

---
+++ src/Components/Producer_Display/ProducerComponent/ProducerIndexComp.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ProducerComp from "./Components/Producer_Display/ProducerComponent/ProducerComp.jsx";
+import data from "../data/data.json";
+import './App.css';
+
+class ProducerIndexComp extends React.Component{
+  constructor(props) {
+    super(props);
+      this.state = {
+        producers: data
+      }
+
+  }
+  render () {
+    return (
+      <>
+        <header>
+          <img src="https://www.dropbox.com/s/zh6tgeyqvnffw33/broadcastCamcorder.jpg?raw=1" className="appPic" alt="Camera Image" />
+          {/* <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+        </header>
+        <section>
+          <ProducerComp producers={this.state.producers} />
+        </section>
+      </>
+    );
+  }
+}
+export default ProducerIndexComp;
