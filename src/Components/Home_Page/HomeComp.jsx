7abdf7ee9eba574c1bd96fa6dfe33bcdbb529eba
Amaka Anikwe <amakaanikwe@gmail.com>
2020-09-30 23:46:19 -0400

    Updated HomeComp.jsx

---
+++ src/Components/Home_Page/HomeComp.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class HomeIndexComp extends React.Component{
+
+  render () {
+    return (
+      <>
+        <header>
+            <h1>Hello World!</h1>
+        </header>
+      </>
+    );
+  }
+}
+export default HomeIndexComp;
