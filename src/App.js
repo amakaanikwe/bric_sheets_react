6aaa3b8912faab6499deaf41289c2f869a446195
Amaka Anikwe <amakaanikwe@gmail.com>
2020-10-04 23:53:49 -0400

    Updated routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route } from 'react-router-dom';
 import HomeIndexComp from "./Components/Home_Page/HomeComp.jsx";
 import About from "./Components/Home_Page/About.jsx";
 import NavBar from "./Components/Home_Page/NavBar.jsx";
+import JobsList from "./Components/Job_Search/JobsList.jsx";
+import JobPage from "./Components/Job_Search/JobPage.jsx";
 import './App.css';
 
 class App extends React.Component{
@@ -14,6 +16,8 @@ class App extends React.Component{
         <NavBar />
         <Route path="/" component={HomeIndexComp} exact/>
         <Route path="/about" component={About} />
+        <Route path="/job-list" component={JobsList} />
+        <Route path="/Job" component={JobPage} />
       </Router>
     );
   }
