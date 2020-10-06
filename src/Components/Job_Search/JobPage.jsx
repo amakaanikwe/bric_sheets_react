c4b799678e62ff9185107ac0c6966a4e18d93044
Amaka Anikwe <amakaanikwe@gmail.com>
2020-10-06 07:21:01 -0400

    Sample file added to project

--- src/Components/Job_Search/JobPage.jsx
+++ src/Components/Job_Search/JobPage.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import samplePosts from './samplePosts';
 
-const Job = ({match}) => {
+const JobPage = ({ match }) => {
+  const name = match.params.name;
+  const job = samplePosts.find(job => job.name === name);
+
+  if (!job) return <h1>Job does not exist!</h1>
+
   return (
     <>
     <header>
-        <h1>Hello Job!</h1>
+        <h1> {job.title} </h1>
+        <p> {job.job_info} </p>
     </header>
   </>
   )
 
 }
 
-export default Job;
+export default JobPage;
