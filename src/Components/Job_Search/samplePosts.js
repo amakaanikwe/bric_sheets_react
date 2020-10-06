c469783badff9507d998dc1d5c1a0bb40ae0d8be
Amaka Anikwe <amakaanikwe@gmail.com>
2020-10-06 07:21:39 -0400

    Sample Posts added to project

---
+++ src/Components/Job_Search/samplePosts.js
@@ -0,0 +1,22 @@
+const job_posts = [
+    {
+        name: "Post_1",
+        title: "Sound Tech",
+        job_info: "In this role we are looking for a qualified sound tech who can help with the sound quiptment on set. Must have own gear."
+
+    },
+    {
+        name: "Post_2",
+        title: "PA",
+        job_info: "In this role we are looking for a hungry go getter PA. Note: Must be really hungry, like use to starving, cause this job does not pay well. Apply today!"
+
+    },
+    {
+        name: "Post_3",
+        title: "Director",
+        job_info: "Are you a Director with a vision? A champion of champions? A leader of men and women? If so come join our existing project and lead us into greatness!!"
+
+    }
+]
+
+export default job_posts;
