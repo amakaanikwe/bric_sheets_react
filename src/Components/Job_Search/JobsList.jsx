7be8680cd090c83b2b1a654a676b81e395ffba50
Amaka Anikwe <amakaanikwe@gmail.com>
2020-10-06 08:01:39 -0400

    Created and linked posts in JobsList

--- src/Components/Job_Search/JobsList.jsx
+++ src/Components/Job_Search/JobsList.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import samplePosts from './samplePosts';
+import { Link } from "react-router-dom";
 
 const JobsList = () => (
     <>
     <header>
         <h1>Hello Jobs List!</h1>
+        {samplePosts.map((job, key) => (
+          <Link key={key} to={`/job/${job.name}`}>
+            <h3>{job.title}</h3>
+            <p>{job.job_info.substring(0, 50)}...</p>
+          </Link>         
+        ))}
     </header>
   </>
 )
