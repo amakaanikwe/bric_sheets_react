a09299381b164b55cb6cd74fd90efe2e7d957f57
Amaka Anikwe <amakaanikwe@gmail.com>
2020-10-05 23:38:24 -0400

    Updated JobPage comp

--- src/Components/Job_Search/JobPage.jsx
+++ src/Components/Job_Search/JobPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const Job = ({match}) => (
+const Job = ({match}) => {
+  return (
     <>
     <header>
         <h1>Hello Job!</h1>
     </header>
   </>
-)
+  )
+
+}
 
 export default Job;
