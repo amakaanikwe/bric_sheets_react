e638021de7185f074c9f622e0dec303247424158
Amaka Anikwe <amakaanikwe@gmail.com>
2020-10-07 06:53:35 -0400

    App.js update - 404 page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import HomeIndexComp from "./Components/Home_Page/HomeComp.jsx";
 import About from "./Components/Home_Page/About.jsx";
 import NavBar from "./Components/Home_Page/NavBar.jsx";
 import JobsListPage from "./Components/Job_Search/JobsListPage.jsx";
 import JobPage from "./Components/Job_Search/JobPage.jsx";
+import NotFoundPage from "./Components/Home_Page/NotFoundPage.jsx";
 import './App.css';
 
 class App extends React.Component{
@@ -13,12 +14,24 @@ class App extends React.Component{
     return (
       <Router>
         <NavBar />
-        <Route path="/" component={HomeIndexComp} exact/>
-        <Route path="/about" component={About} />
-        <Route path="/job-list" component={JobsListPage} />
-        <Route path="/Job/:name" component={JobPage} />
+        <Switch>
+          <Route path="/" component={HomeIndexComp} exact/>
+          <Route path="/about" component={About} />
+          <Route path="/job-list" component={JobsListPage} />
+          <Route path="/Job/:name" component={JobPage} />
+          {/* 
+            if you don't give a route a path like below, it will always match by default .
+            Switch makes sure only one route is rendered at a time. Not Found page needs to be last.
+          */}
+          <Route component={NotFoundPage} />
+        </Switch>
+        
+
       </Router>
     );
   }
 }
 export default App;
+
+
+// 
