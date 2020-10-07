c20efb5e6582920ca9ceeba6abf51aa87c745f5c
Amaka Anikwe <amakaanikwe@gmail.com>
2020-10-07 06:28:16 -0400

    JobPost deleted - making posts more modular

--- src/Components/Job_Search/JobPage.jsx
+++ src/Components/Job_Search/JobPage.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import samplePosts from './samplePosts';
+import JobsListComp from '../ReuseComponents/JobsListComp.jsx'
 
 const JobPage = ({ match }) => {
   const name = match.params.name;
   const job = samplePosts.find(job => job.name === name);
 
+
   if (!job) return <h1>Job does not exist!</h1>
+  
+  // Will grab all posts excep the one you are on
+  const otherPosts = samplePosts.filter(post => post.name !== name);
 
   return (
     <>
     <header>
         <h1> {job.title} </h1>
         <p> {job.job_info} </p>
+        <h3>Other Posts</h3>
+        {/* Shows how to reuse comp using diff prop */}
+        <JobsListComp posts={ otherPosts } />
     </header>
   </>
   )
