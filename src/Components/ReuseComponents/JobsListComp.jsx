c6fb29669178ff9b56fc6343ef98812d606964b1
Amaka Anikwe <amakaanikwe@gmail.com>
2020-10-07 06:31:49 -0400

    Reusable comp update - making posts more modular

---
+++ src/Components/ReuseComponents/JobsListComp.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const JobsList = ({ posts }) => (
+    <>
+        {posts.map((job, key) => (
+            <Link key={key} to={`/job/${job.name}`}>
+            <h3>{job.title}</h3>
+            <p>{job.job_info.substring(0, 50)}...</p>
+            </Link>         
+        ))}
+    </>
+);
+
+export default JobsList; 
