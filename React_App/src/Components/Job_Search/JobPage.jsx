472253418a495c11a26f0f6a1b23423995236cee
Amaka Anikwe <amakaanikwe@gmail.com>
2020-10-20 23:52:33 -0400

    JobPage update - useEffect hook added.

--- React_App/src/Components/Job_Search/JobPage.jsx
+++ React_App/src/Components/Job_Search/JobPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import samplePosts from './samplePosts';
 import JobsListComp from '../ReuseComponents/JobsListComp.jsx'
 import NotFoundPage from "../Home_Page/NotFoundPage.jsx";
@@ -8,6 +8,17 @@ const JobPage = ({ match }) => {
   const name = match.params.name;
   const job = samplePosts.find(job => job.name === name);
 
+  const [jobInfo, setJobInfo] = useState({ upvotes:0, comments: [] });
+
+  // standard fetch set up
+  useEffect (() => {
+    const fetchData = async () => {
+        const result = await fetch(`http://localhost:8000/api/job/${name}`)
+        const body = await result.json();
+        setJobInfo(body);
+    }
+    fetchData();
+  }, [name])
 
   if (!job) return <NotFoundPage />
   
@@ -18,6 +29,7 @@ const JobPage = ({ match }) => {
     <>
     <header>
         <h1> {job.title} </h1>
+        <p>Upvotes {jobInfo.upvotes} </p>
         <p> {job.job_info} </p>
         <h3>Other Posts</h3>
         {/* Shows how to reuse comp using diff prop */}
