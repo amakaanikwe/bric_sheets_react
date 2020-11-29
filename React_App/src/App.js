fa0ec50452badda1ad3f4f7b9a4e32d347b58d5b
Amaka Anikwe <amakaanikwe@gmail.com>
2020-11-29 10:41:31 -0500

    App.js links update

--- React_App/src/App.js
+++ React_App/src/App.js
@@ -5,6 +5,7 @@ import About from "./Components/Home_Page/About.jsx";
 import NavBar from "./Components/Home_Page/NavBar.jsx";
 import JobsListPage from "./Components/Job_Search/JobsListPage.jsx";
 import JobPage from "./Components/Job_Search/JobPage.jsx";
+import ProducerPage from "./Components/Producer_Display/ProducerComponent/ProducerIndexComp.jsx";
 import NotFoundPage from "./Components/Home_Page/NotFoundPage.jsx";
 import './App.css';
 
@@ -19,6 +20,7 @@ class App extends React.Component{
           <Route path="/about" component={About} />
           <Route path="/job-list" component={JobsListPage} />
           <Route path="/job/:name" component={JobPage} />
+          <Route path="/producer-display" component={ProducerPage} />
           {/* 
             if you don't give a route a path like below, it will always match by default .
             Switch makes sure only one route is rendered at a time. Not Found page needs to be last.
