134a6d13c2bb1d2e7f59fe366de08a0ef46861fb
Amaka Anikwe <amakaanikwe@gmail.com>
2021-01-05 22:44:03 -0500

    Updated Producer_Template

--- React_App/src/Components/Producer_Display/ProducerComponent/ProducerTemplate.jsx
+++ React_App/src/Components/Producer_Display/ProducerComponent/ProducerTemplate.jsx
@@ -6,14 +6,14 @@ class ProducerTempComp extends React.Component{
     render () {
       return (
           <>
-            <section className="container containerStyle borderBottom">
+            <section className="producerTempStyle">
             <div className="row">
                 <div className="col-lg-9">
                 <h3>{this.props.producer.firstName + " " + this.props.producer.lastName}</h3>
                 </div>
                 <div className="col-lg-3">{this.props.producer.emailAddress}</div>
             </div>
-            <div className="row">
+            <div className="row producerListStyle">
                 <h5 className="col-lg-12">Bric Certifications</h5>
                 <div className="col-lg-8">
                 <ul>
@@ -25,16 +25,14 @@ class ProducerTempComp extends React.Component{
                 <ul>
                     <li>{this.props.producer.workExperience}</li>
                 </ul>
-                <div>
+        </div>
+ 
+        </div>
+        <div>
                 <a>
-                <button type="button" className="btn-info">details</button>
+                <button type="button" className="btn-info">more details</button>
             </a>
             </div>
-        </div>
-        </div>
-        </section>
-        <section class="container" >
-        <pagination uib-pagination ng-model="currentPage" total-items="producers.length" max-size="maxSize" items-per-page="numPerPage" boundary-links="true"  class="pagination-sm pagination" previous-text="&lsaquo;" next-text="&rsaquo;" first-text="&laquo;" last-text="&raquo;"></pagination>
         </section>
     </>
       )
