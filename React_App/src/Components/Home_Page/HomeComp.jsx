c07c9ff1e72dfe343b0d52c531f5faa3d6444a01
Amaka Anikwe <amakaanikwe@gmail.com>
2021-01-15 06:54:00 -0500

    Refactored Home component

--- React_App/src/Components/Home_Page/HomeComp.jsx
+++ React_App/src/Components/Home_Page/HomeComp.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import {TweenMax, Power3} from 'gsap';
+import backLogo from './warren-wong-pfmaMGZSby4-web.jpg'
 
 
-class HomeIndexComp extends React.Component{
+const HomeIndexComp = () => {
 
-  render () {
+
     return (
       <>
         <header className="container header">
             <h1>Hello World!</h1>
         </header>
+        <secton>
+          <div className='hero'>
+            <img className='homeImage' src={backLogo} alt='film camera'/>
+          </div>
+        </secton>
       </>
     );
-  }
+  
 }
+
 export default HomeIndexComp;
