76c87a7da073ec168563677d9cf7dacdf345f22f
Amaka Anikwe <amakaanikwe@gmail.com>
2021-01-17 16:21:47 -0500

    Updated HomeComp - gsap animation set up

--- React_App/src/Components/Home_Page/HomeComp.jsx
+++ React_App/src/Components/Home_Page/HomeComp.jsx
@@ -1,9 +1,17 @@
-import React, {useRef} from 'react';
-import {TweenMax, Power3} from 'gsap';
+import React, {useRef, useEffect} from 'react';
+import {TweenMax, TimelineMax, Power3} from 'gsap';
 import backLogo from './warren-wong-pfmaMGZSby4-web.jpg'
 
 
 const HomeIndexComp = () => {
+  let heroRef = useRef(null)
+  let sliderRef = useRef(null)
+  let headlineRef = useRef(null)
+
+  useEffect( ()=>{
+
+  }, []);
+// heroRef.style.display='none'
 
 
     return (
@@ -11,15 +19,15 @@ const HomeIndexComp = () => {
         <header className="container header">
             <h1>Hello World!</h1>
           <secton>
-            <div className='hero'>
+            <div className='hero' ref={hero=>{heroRef=hero}} >
               <img className='homeImage' src={backLogo} alt='film camera'/>
-              <h1 className='headline'>Filmmakers Portal</h1>
-              <button>Enter</button>
+              <h1 className='headline' ref={head=>{headlineRef=head}}>Filmmakers' Portal</h1>
+              <button>Log In</button>
             </div>
           </secton>
         </header>
 
-        <div className='slider'></div>
+        <div className='slider' ref={slide=>{sliderRef=slide}}></div>
         
       </>
     );
