44da8dca0fd28c459d86d5368002b71c5869f1d1
Amaka Anikwe <amakaanikwe@gmail.com>
2021-01-18 22:38:09 -0500

    Updated footer

---
+++ React_App/src/Components/Home_Page/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+const Footer = () => (
+<>
+    <div className='footer'>
+        <p>Footer</p>
+    </div>
+</>
+);
+
+export default Footer;
