c1873b3ad180adc885d74f656983d49b57d13d0c
Amaka Anikwe <amakaanikwe@gmail.com>
2021-01-20 05:13:38 -0500

    Updated HomeComp - Animation added

--- React_App/src/Components/Home_Page/HomeComp.jsx
+++ React_App/src/Components/Home_Page/HomeComp.jsx
@@ -1,29 +1,44 @@
 import React, {useRef, useEffect} from 'react';
 import {TweenMax, TimelineMax, Power3} from 'gsap';
+import Login from'./Login.jsx'
 import backLogo from './warren-wong-pfmaMGZSby4-web.jpg'
+import './HomeCompStyle.css'
 
 
 const HomeIndexComp = () => {
+
   let heroRef = useRef(null)
   let sliderRef = useRef(null)
   let headlineRef = useRef(null)
+  let homeGridRef = useRef(null)
+  const tl = new TimelineMax();
 
   useEffect( ()=>{
-
+    tl.fromTo(heroRef,1,{height:"0%"}, {height:"80%", ease: Power3.easeInOut})
+    .fromTo(heroRef,1.2, {width:'100%'}, {width: '80%', ease: Power3.easeInOut}) 
+    .fromTo(sliderRef, 1.2, {x: '-100%'}, {x: '0%', ease: Power3.easeInOut})
+    .fromTo(headlineRef, 0.5, {opacity: 0, x:30}, {opacity:1, x:0}, '-=0.5')
+    .fromTo(homeGridRef, 0.5, {opacity: 0, x:-30}, {opacity:1, x:0}, '-=0.5')
   }, []);
 // heroRef.style.display='none'
 
 
     return (
       <>
-        <header className="container header">
-            <h1>Hello World!</h1>
-          <secton>
+        <header className="homeCompStyle">
+          <secton className='homeSection'>
+
             <div className='hero' ref={hero=>{heroRef=hero}} >
               <img className='homeImage' src={backLogo} alt='film camera'/>
-              <h1 className='headline' ref={head=>{headlineRef=head}}>Filmmakers' Portal</h1>
-              <button>Log In</button>
+              <div className='textBorder'>
+                  <h1 className='headline textBorder' ref={head=>{headlineRef=head}}>Filmmaker's Portal</h1>
+              </div>
+              <div className='homeGrid' ref={grid=>{homeGridRef=grid}} >
+              < Login />  
+              </div>
+                           
             </div>
+
           </secton>
         </header>
 
