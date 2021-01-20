a3a88d603006d3575478c1cdb114f7689075acb3
Amaka Anikwe <amakaanikwe@gmail.com>
2021-01-20 10:09:40 -0500

    updated sample posts

--- React_App/src/Components/Job_Search/samplePosts.js
+++ React_App/src/Components/Job_Search/samplePosts.js
@@ -2,19 +2,19 @@ const job_posts = [
     {
         name: "Post_1",
         title: "Sound Tech",
-        job_info: "In this role we are looking for a qualified sound tech who can help with the sound quiptment on set. Must have own gear."
+        job_info: "We are looking for a professional videographer / Gaffer and Sound Designer. Compensation based on experience - please reply with your movie portfolio and we will get back ASAP. PLEASE DISREGARD THIS ADD IF YOUR AN AMATEUR, OR YOU DON'T HAVE EXPERIENCE IN CINEMATOGRAPHY."
 
     },
     {
         name: "Post_2",
         title: "PA",
-        job_info: "In this role we are looking for a hungry go getter PA. Note: Must be really hungry, like use to starving, cause this job does not pay well. Apply today!"
+        job_info: "In this role we are looking for a hungry go getter PA. Food photography studio looking for studio assistant with car and laptop. Be a self-starter and able to take a look at what needs to be done and move forward. Requirements: Strong, attentive work ethic. The ability to be given a task and complete it with little or no supervision."
 
     },
     {
         name: "Post_3",
         title: "Director",
-        job_info: "Are you a Director with a vision? A champion of champions? A leader of men and women? If so come join our existing project and lead us into greatness!!"
+        job_info: "Seeking a legitimate film director. (this is not a job offer) I'm searching for a serious individual who genuinely wants to produce quality low budget films or a theatrical play. I'm an award-winning screenwriter with several viable screenplays. They are all high concept, low budget features that can be shot in New York."
 
     }
 ]
