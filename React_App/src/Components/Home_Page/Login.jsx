7121df20b9ab68bf4a18be49df9aad0c1f19eff7
Amaka Anikwe <amakaanikwe@gmail.com>
2021-01-20 05:14:52 -0500

    Login comp added

---
+++ React_App/src/Components/Home_Page/Login.jsx
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import './HomeCompStyle.css'
+
+const Login = () => {
+
+    // Form Handler
+  const initialState = { username:"", age:""}
+  const [entry, setEntry] = useState(initialState);
+  const {username, age} = entry;
+  const formChangeHandler = (e) => {
+    let name = e.target.name;
+    let val = e.target.value;
+
+    setEntry({...entry, [name]: val});
+    }
+
+    return(
+        <>
+        {/* Grid container */}
+        <div className='grid-container'>
+
+        <div className='section1'>
+        <input
+            className='homeUser'
+            type='text'
+            name='username'
+            value='Username'
+            onChange={formChangeHandler}/>
+        </div>
+        <div className='section2'>
+        <input
+            className='homePass'
+            type='text'
+            name='password'
+            value='Password'
+            onChange={formChangeHandler}/> 
+        </div>
+        <div className='section3'>
+        <button className='homeButton'>Log In</button>
+        </div>                     
+        </div> {/* end of grid */}
+
+    </>
+    )
+   
+}
+
+export default Login;
+
