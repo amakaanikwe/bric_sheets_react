d9f3c426f09506bfca0b1a234b6cfa8d7d16ffbd
Amaka Anikwe <amakaanikwe@gmail.com>
2021-01-31 22:07:41 -0500

    Added CreateJobPostModel

---
+++ React_App_Backend/src/models/CreateJobPostModel.js
@@ -0,0 +1,36 @@
+// use mongoose to create a schema
+const mongoose = require('mongoose');
+
+const jobPostTemplate = new mongoose.Schema({
+    company:{
+        type:String,
+        requiered:true
+    },
+    jobTitle:{
+        type:String,
+        requiered:true
+    },    
+    jobType:{
+        type:String,
+        requiered:true
+    },    
+    jobDetails:{
+        type:String,
+        requiered:true
+    },
+    phoneNumber:{
+        type:String,
+        requiered:true
+    },
+    email:{
+        type:String,
+        requiered:true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    },
+
+})
+
+module.exports = mongoose.model('newUserTable', jobPostTemplate);
