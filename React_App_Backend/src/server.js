c8feba6b1f76c8aba744eca26de850810cc93997
Amaka Anikwe <amakaanikwe@gmail.com>
2021-02-01 06:27:39 -0500

    Updated server

--- React_App_Backend/src/server.js
+++ React_App_Backend/src/server.js
@@ -4,7 +4,7 @@ import { MongoServerSelectionError } from 'mongodb';
 const mongoose = require('mongoose');
 // require('dotenv').config({ path: './env' })
 const dotenv = require('dotenv');
-const routesUrls = require('./routes/routes');
+const signup = require('./routes/signup');
 const cors = require('cors');
 
 dotenv.config();
@@ -13,7 +13,7 @@ mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("Database is con
 
 app.use(express.json());
 app.use(cors());
-app.use('/api', routesUrls);
+app.use('/api', signup);
 
 const port = process.env.PORT || 4000;
 app.listen(port, ()=> console.log(`server is running on ${port}...`))
