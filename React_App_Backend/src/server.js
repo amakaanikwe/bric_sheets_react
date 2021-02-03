ce85bd9840101c8bf91abfcfc5f4e79b506a2f81
Amaka Anikwe <amakaanikwe@gmail.com>
2021-02-03 06:46:56 -0500

    Updated server file

--- React_App_Backend/src/server.js
+++ React_App_Backend/src/server.js
@@ -4,22 +4,47 @@ import { MongoServerSelectionError } from 'mongodb';
 const mongoose = require('mongoose');
 // require('dotenv').config({ path: './env' })
 const dotenv = require('dotenv');
-const signup = require('./routes/signup');
-const postJobs = require('./routes/postJobs');
 const cors = require('cors');
+const passport = require('passport');
+const passportlocal = require('passport-local').strategy;
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+const signup = require('./routes/signup');
+// const postJobs = require('./routes/postJobs');
 
 dotenv.config();
+//---------------------------- END OF IMPORTS  ---------------------------------
 
 mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("Database is connected"));
 
-app.use(express.json());
-app.use(cors());
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors({
+    origin: 'http://localhost:4000', //<-- Location of react app connecting to
+    credentials: true
+}));
 
-// Registration endpoint
+app.use(session({
+    secret: 'secretcode',
+    resave: true,
+    saveUninitialized: true
+}))
+
+app.use(cookieParser('secretcode'))
+
+// API endpoints
 app.use('/api', signup);
+// app.use('/api', postJobs);
 
-// Jobs Post endpoint
-app.use('/api', postJobs);
+app.post('/api/login', (req,res) => {
+
+})
+
+
 
 const port = process.env.PORT || 4000;
 app.listen(port, ()=> console.log(`server is running on ${port}...`))
