3b98e2e2421413a4225f0363773c275507187e61
Amaka Anikwe <amakaanikwe@gmail.com>
2021-02-03 06:47:39 -0500

    Updated postJobs file

--- React_App_Backend/src/routes/postJobs.js
+++ React_App_Backend/src/routes/postJobs.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const createJobTemplateCopy = require('../models/CreateJobModel');
+const createJobTemplateCopy = require('../models/CreateJobPostModel');
 
 router.post("/postjob", (req, res)=>{
     const newPost = new createJobTemplateCopy ({
