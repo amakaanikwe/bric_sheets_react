2c1c3678507262fd622f62da3b061077f0512c0f
Amaka Anikwe <amakaanikwe@gmail.com>
2021-02-03 06:45:55 -0500

    Updated login component

--- React_App/src/Components/Home_Page/Login.jsx
+++ React_App/src/Components/Home_Page/Login.jsx
@@ -4,15 +4,17 @@ import './HomeCompStyle.css'
 const Login = () => {
 
     // Form Handler
-  const initialState = { username:"", age:""}
+  const initialState = { username:"", password:""}
   const [entry, setEntry] = useState(initialState);
-  const {username, age} = entry;
+  const {username, password} = entry;
   const formChangeHandler = (e) => {
     let name = e.target.name;
     let val = e.target.value;
 
     setEntry({...entry, [name]: val});
     }
+
+const login = () => {}
 
     return(
         <>
@@ -35,7 +37,7 @@ const Login = () => {
             onChange={formChangeHandler}/> 
         </div>
         <div className='section3'>
-        <button className='homeButton'>Log In</button>
+        <button className='homeButton' onClick={login}>Log In</button>
         </div>                     
         </div> 
 
