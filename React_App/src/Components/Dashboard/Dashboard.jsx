af26a312d068fd7337087347e606b9163f7f9be7
Amaka Anikwe <amakaanikwe@gmail.com>
2021-02-07 00:08:14 -0500

    Updated Dashboard

--- React_App/src/Components/Dashboard/Dashboard.jsx
+++ React_App/src/Components/Dashboard/Dashboard.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import './dashboard.css';
 import NavBar from '../Home_Page/NavBar';
+import PostJob from "./Components/Post_Job/PostJob.jsx";
+import JobsListPage from "./Components/Job_Search/JobsListPage.jsx";
+import JobPage from "./Components/Job_Search/JobPage.jsx";
+import ProducerPage from "./Components/Producer_Display/ProducerComponent/ProducerIndexComp.jsx";
 import Footer from '../Home_Page/Footer';
 
 const Dashboard = () => {
 
+    return (
+        <>
+            <NavBar />
+            
+
+            <Footer />
+        </>
+    )
 }
 
 export default Dashboard;
