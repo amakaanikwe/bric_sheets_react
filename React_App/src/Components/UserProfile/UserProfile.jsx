fc51dedc75163bdaa949bb9d8e03bf92286e8242
Amaka Anikwe <amakaanikwe@gmail.com>
2021-02-08 22:24:38 -0500

    Added UserProfile comp

---
+++ React_App/src/Components/UserProfile/UserProfile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './userProfile.css';
+
+
+const UserProfile = () => {
+
+    return (
+        <>
+            <NavBar />
+            
+
+            <Footer />
+        </>
+    )
+}
+
+export default UserProfile;
