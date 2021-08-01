d9c9d527ccc2d50dd10aab5d3778a82f730e4d2a
Amaka Anikwe <amakaanikwe@gmail.com>
2021-07-31 21:21:55 -0400

    Updated app.js

--- React_App/src/App.js
+++ React_App/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+// --------------   IMPORTED COMPONENTS  ------------------
 import HomeIndexComp from "./Components/Home_Page/HomeComp.jsx";
 import PostJob from "./Components/Post_Job/PostJob.jsx";
 import NavBar from "./Components/Home_Page/NavBar.jsx";
 import JobsListPage from "./Components/Job_Search/JobsListPage.jsx";
 import JobPage from "./Components/Job_Search/JobPage.jsx";
 import ProducerPage from "./Components/Producer_Display/ProducerComponent/ProducerIndexComp.jsx";
+import Registration from "./Components/Home_Page/Registration.jsx";
 import NotFoundPage from "./Components/Home_Page/NotFoundPage.jsx";
 import './App.css';
 
@@ -20,6 +23,7 @@ const App =()=> {
           <Route path="/job/:name" component={JobPage} />
           <Route path="/producer-display" component={ProducerPage} />
           <Route path="/post-job" component={PostJob} />
+          <Route path="/registration" component={Registration} />
           {/* 
             if you don't give a route a path like below, it will always match by default.
             Switch makes sure only one route is rendered at a time. Not Found page needs to be last.
